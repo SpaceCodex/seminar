07c51b29aeb0be18ed22092a0894bef48163bdf9
Vardhan Suryavanshi <vardhan.suryavanshi64@gmail.com>
2024-10-19 18:03:04 +0530

    Added new line in Past Code

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
     echo "My first PHP script!";
     echo "I'm learing Git.....";
     echo "The standard Lorem Ipsum passage, used since the 1500s";
+    echo "1914 translation by H. Rackham (Past Code)";
 ?>
